fdad25eb5b102ac7ac5509d4f14f41b08bf0d979
Ryota Kobayashi <rytakov@gmail.com>
2017-07-17 01:38:14 +0900

    Add tests

--- test/def.fish
+++ test/def.fish
@@ -1,9 +1,30 @@
+#
+# setup & teardown
+#
+
 function setup
-    set -g __test_def_function_path (mktemp -d)
-    set -g __test_def_complete_path (mktemp -d)
+    set -g __test_def_function_path (realpath (mktemp -d))
+    set -g __test_def_complete_path (realpath (mktemp -d))
 
     set -g def_function_path "$__test_def_function_path"
     set -g def_complete_path "$__test_def_complete_path"
+    set -g fish_complete_path "$__test_def_complete_path"
+
+    function __test_def_function_editor -a path
+        set -l name (basename "$path" .fish)
+        begin
+            echo "function $name"
+            echo "  echo $name"
+            echo "end"
+        end > "$path"
+    end
+
+    function __test_def_completion_editor -a path
+        set -l name (basename "$path" .fish)
+        echo "complete -c $name -s a" > "$path"
+    end
+
+    set -g EDITOR __test_def_function_editor
 end
 
 function teardown
@@ -11,10 +32,57 @@ function teardown
     rm -rf "$__test_def_complete_path"
 end
 
-test "root directory for functions"
-    "$__test_def_function_path" = (def -r)
+#
+# tests
+#
+
+test "--help option"
+    (count (def --help)) -gt 0
 end
 
-test "root directory for completions"
-    "$__test_def_complete_path" = (def -cr)
+test "--root option"
+    "$__test_def_function_path" = (def --root)
 end
+
+test "--root --complete option"
+    "$__test_def_complete_path" = (def --complete --root)
+end
+
+test "edit a new function"
+    (def foo) foo = (foo) (functions -e foo)
+end
+
+test "edit a new completion"
+    (set -g EDITOR __test_def_completion_editor
+    def --complete foo) "-a" = (complete -C"foo -")
+end
+
+test "--list option"
+    (def foo; def bar) foo bar = (def --list) (functions -e foo bar)
+end
+
+test "--list --complete option"
+    (set -g EDITOR __test_def_completion_editor
+    def --complete foo
+    def --complete bar) foo bar = (def --list --complete)
+end
+
+test "--erase option"
+    (def foo; def --erase foo) 0 = (count (def --list))
+end
+
+test "--erase --complete option"
+    (set -g EDITOR __test_def_completion_editor
+    def --complete foo
+    def --erase --complete foo) 0 = (count (def --list --complete))
+end
+
+test "--force option"
+    (def --force def) def = (def) (fisher update def ^/dev/null)
+end
+
+test "--force --complete option"
+    (set -g EDITOR __test_def_completion_editor
+    def --force --complete def) "-a" = (complete -C"def -") (fisher update def ^/dev/null)
+end
+
